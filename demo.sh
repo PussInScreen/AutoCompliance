b2a013329332f2b658cc706089aaab309f1d1290
Andrew <19553807+andrewk10@users.noreply.github.com>
2022-08-28 13:52:12 +0100

    [FIX] Demo.sh file being transferred was at the incorrect path

--- demo.sh
+++ demo.sh
@@ -15,7 +15,7 @@ sudo ./autocompliance/src/demo.py -t autocompliance/src/test_files/ip_list_short
 echo "Running the propagation script just across SSH and spreading a specific file"
 sudo ./autocompliance/src/demo.py -t autocompliance/src/test_files/ip_list_short.txt -p \
 22 -u root -f autocompliance/src/test_files/passwords_list_short.txt -d \
-src/test_files/file.txt
+autocompliance/src/test_files/file.txt
 
 # Running the propagation script across SSH but acquiring IPs through a local
 # scan and then subsequently self propagating.
